acd215134de2bbe01c61a73c88d6a8670603e510
Silas Davis <silas.davis@monaxlabs.io>
2023-01-03 11:29:08 +0100

    Split into separate examples and sdk workspaces
    Add react app
    
    Signed-off-by: Silas Davis <silas.davis@monaxlabs.io>

---
+++ examples/react/README.md
@@ -0,0 +1,23 @@
+# Aspen Publishing SDK's examples
+
+The examples are created in `Next.js`. In `index.tsx` we can find snippets of the code on how to call the contracts, the function associated and the parameters that they need.
+
+## Getting Started
+
+### Step 1: Install dependencies
+
+```
+   $ yarn install
+```
+
+### Step 2: Make it run
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ sdk/src/contracts/providers.ts
@@ -0,0 +1,69 @@
+import { providers, Wallet } from 'ethers';
+import * as t from 'io-ts';
+import { JsonFromString } from 'io-ts-types';
+import { URL } from 'url';
+import { parseFromEnvOrFile } from '../environment';
+
+// A JSON object of type EnvCredentials is expected to be saved here (ignored from repo)
+const defaultConfigFile = new URL('providers.json', import.meta.url).pathname;
+const defaultConfigEnvVarName = 'PROVIDERS_JSON';
+
+const NetworkSecrets = t.partial({
+  Mumbai: t.string,
+  Mainnet: t.string,
+  Goerli: t.string,
+  Polygon: t.string,
+});
+
+type NetworkSecrets = t.TypeOf<typeof NetworkSecrets>;
+
+const ProviderConfig = t.type({
+  providerUrls: NetworkSecrets,
+  privateKeys: NetworkSecrets,
+});
+
+type ProviderConfig = t.TypeOf<typeof ProviderConfig>;
+
+let providerConfig: ProviderConfig;
+
+export type SupportedNetwork = keyof NetworkSecrets;
+export const SupportedNetwork = t.keyof(NetworkSecrets.props);
+
+export const supportedNetworks = Object.freeze(Object.keys(NetworkSecrets.props)) as SupportedNetwork[];
+
+export async function getProvider(network: SupportedNetwork, chainId?: number): Promise<providers.JsonRpcProvider> {
+  const { providerUrls } = await getProviderConfig();
+  const providerUrl = providerUrls[network];
+  if (!providerUrl) {
+    throw new Error(`No provider URL provided for network ${network}`);
+  }
+  return new providers.JsonRpcProvider(providerUrl, chainId);
+}
+
+export async function getSigner(network: SupportedNetwork): Promise<Wallet> {
+  const { privateKeys } = await getProviderConfig();
+  const provider = await getProvider(network);
+  const privateKey = privateKeys[network];
+  if (!privateKey) {
+    throw new Error(`No private key provided for network ${network}`);
+  }
+  return new Wallet(privateKey, provider);
+}
+
+export async function initialiseProvider(
+  configFile = defaultConfigFile,
+  configEnvVarName = defaultConfigEnvVarName,
+): Promise<void> {
+  // FIXME: contain this state in some kind of SDK object rather tha global state
+  await getProviderConfig(configFile, configEnvVarName);
+}
+
+async function getProviderConfig(
+  configFile = defaultConfigFile,
+  configEnvVarName = defaultConfigEnvVarName,
+): Promise<ProviderConfig> {
+  if (!providerConfig) {
+    providerConfig = await parseFromEnvOrFile(JsonFromString.pipe(ProviderConfig), configFile, configEnvVarName);
+  }
+  return providerConfig;
+}
