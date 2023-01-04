28a826eae001698192c2d7cb3b59b985da59622f
Silas Davis <silas.davis@monaxlabs.io>
2023-01-04 17:29:56 +0100

    Pull in feature factories code generation
    Signed-off-by: Silas Davis <silas.davis@monaxlabs.io>

--- sdk/lib/cmd/generate-from-spec.ts
+++ sdk/lib/cmd/generate-from-spec.ts
@@ -1,21 +1,33 @@
 import * as path from 'path';
-import { dumpLatestABIs, generateTsFile } from '../spec';
-// const dirname = __dirname
 import * as url from 'url';
+import { ContractsManifest } from '../../src/manifest';
+import { parse } from '../../src/schema';
+import { dumpLatestABIs, generateTsFile, writeFeaturesFactoriesMap } from '../generate';
 const dirname = url.fileURLToPath(new url.URL('.', import.meta.url));
 
-export const rootDir = path.join(dirname, '..', '..');
-const srcDir = path.join(rootDir, 'src');
+export const sdkDir = path.join(dirname, '..', '..');
+const srcDir = path.join(sdkDir, 'src');
 const deployersFileTs = path.join(srcDir, 'deployers.gen.ts');
 const manifestFileTs = path.join(srcDir, 'manifest.gen.ts');
-export const contractsAbiDir = path.join(rootDir, 'abis');
+const featureFactoriesTs = path.join(srcDir, 'feature-factories.gen.ts');
+export const contractsAbiDir = path.join(sdkDir, 'abis');
+export const specDir = path.join(dirname, '..', '..', 'node_modules', '@monaxlabs', 'spec');
+const pathToManifestJson = path.join(specDir, 'contracts/manifest.json');
+const prettierConfigFile = path.join(sdkDir, '.prettierrc');
 
-generateTsFile('deployers', 'contracts/deployments.json', deployersFileTs)
-  .then(() => generateTsFile('manifest', 'contracts/manifest.json', manifestFileTs))
-  .then(() => Promise.all([dumpLatestABIs(contractsAbiDir)]))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+async function generate(): Promise<void> {
+  await generateTsFile(
+    'deployers',
+    path.join(specDir, 'contracts/deployments.json'),
+    prettierConfigFile,
+    deployersFileTs,
+  );
+  const manifestJson = await generateTsFile('manifest', pathToManifestJson, prettierConfigFile, manifestFileTs);
+  await Promise.all([dumpLatestABIs(contractsAbiDir, pathToManifestJson)]);
+  await writeFeaturesFactoriesMap(parse(ContractsManifest, manifestJson), prettierConfigFile, featureFactoriesTs);
+}
 
-export {}
+generate().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
