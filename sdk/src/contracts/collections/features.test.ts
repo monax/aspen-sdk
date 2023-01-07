dd83cbf786d7e2f7ff4f8bd6495274362766644b
Silas Davis <silas.davis@monaxlabs.io>
2023-01-08 00:14:13 +0100

    Combine CI build and test
    Signed-off-by: Silas Davis <silas.davis@monaxlabs.io>

--- sdk/src/contracts/collections/features.test.ts
+++ sdk/src/contracts/collections/features.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { exhaustiveUnionPartitioner } from './features';
 
 describe('Features', () => {
