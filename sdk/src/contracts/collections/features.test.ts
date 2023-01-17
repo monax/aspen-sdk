f90d337160ef7ae9e84e59550f12cb6fca31a98b
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2023-01-17 11:37:54 +0200

    Update partitioner functiontest

--- sdk/src/contracts/collections/features.test.ts
+++ sdk/src/contracts/collections/features.test.ts
@@ -6,8 +6,9 @@ describe('Features', () => {
     const map = { tree: 1, flower: 2, cactus: 9, vine: 6 } as const;
     const partitioner = exhaustiveUnionPartitioner('tree', 'cactus', 'vine', 'reed');
     // This is a compile-time error if we do not have every string on the line above in some subset
-    const { one, two } = partitioner({ one: ['cactus'], two: ['vine'], three: ['reed', 'tree'] })(map);
+    const { one, two, three } = partitioner({ one: ['cactus'], two: ['vine', 'tree'], three: ['reed'] })(map);
     expect(one).toEqual(9);
     expect(two).toEqual(6);
+    expect(three).toEqual(undefined);
   });
 });
