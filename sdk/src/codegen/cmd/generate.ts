0f5a23f1ec71942e9915917d80726cc49e6c0bd6
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2023-02-17 10:50:52 +0200

    Update getDeployer function

--- sdk/src/codegen/cmd/generate.ts
+++ sdk/src/codegen/cmd/generate.ts
@@ -1,7 +1,14 @@
 import * as path from 'path';
 import * as url from 'url';
 import { parse } from '../../utils';
-import { dumpLatestABIs, generateTsFile, writeFeaturesFactoriesMap, writeFeaturesFunctionsMap } from '../generate';
+import { DeployersManifest } from '../deployers';
+import {
+  dumpLatestABIs,
+  generateTsFile,
+  writeDeployerFactoriesMap,
+  writeFeaturesFactoriesMap,
+  writeFeaturesFunctionsMap,
+} from '../generate';
 import { ContractsManifest } from '../manifest';
 
 const dirname = url.fileURLToPath(new url.URL('.', import.meta.url));
@@ -10,26 +17,28 @@ const sdkDir = path.join(dirname, '..', '..', '..');
 const srcDir = path.join(sdkDir, 'src');
 
 const manifestFileTs = path.join(srcDir, 'codegen', 'manifest.gen.ts');
-const deployersFileTs = path.join(srcDir, 'contracts', 'deployers.gen.ts');
+const deployersFileTs = path.join(srcDir, 'contracts', 'deployer', 'deployers.gen.ts');
+const deployerFactoriesTs = path.join(srcDir, 'contracts', 'deployer', 'deployer-factories.gen.ts');
 const contractsAbiDir = path.join(srcDir, 'contracts', 'abis');
 const featureFactoriesTs = path.join(srcDir, 'contracts', 'collections', 'features', 'feature-factories.gen.ts');
 const featureFunctionsTs = path.join(srcDir, 'contracts', 'collections', 'features', 'feature-functions.gen.ts');
 
 const specDir = path.join(sdkDir, 'node_modules', '@monaxlabs', 'spec');
+const pathToDeploymentsJson = path.join(specDir, 'contracts/deployments.json');
 const pathToManifestJson = path.join(specDir, 'contracts/manifest.json');
 const prettierConfigFile = path.join(sdkDir, '.prettierrc');
 
 async function generate(): Promise<void> {
-  await generateTsFile(
-    'deployers',
-    path.join(specDir, 'contracts/deployments.json'),
-    prettierConfigFile,
-    deployersFileTs,
-  );
-  const manifestJson = await generateTsFile('manifest', pathToManifestJson, prettierConfigFile, manifestFileTs);
   await Promise.all([dumpLatestABIs(contractsAbiDir, pathToManifestJson)]);
-  await writeFeaturesFactoriesMap(parse(ContractsManifest, manifestJson), prettierConfigFile, featureFactoriesTs);
-  await writeFeaturesFunctionsMap(parse(ContractsManifest, manifestJson), prettierConfigFile, featureFunctionsTs);
+
+  const manifestJson = await generateTsFile('manifest', pathToManifestJson, prettierConfigFile, manifestFileTs);
+  const manifest = parse(ContractsManifest, manifestJson);
+  await writeFeaturesFactoriesMap(manifest, prettierConfigFile, featureFactoriesTs);
+  await writeFeaturesFunctionsMap(manifest, prettierConfigFile, featureFunctionsTs);
+
+  const deploymentsJson = await generateTsFile('deployers', pathToDeploymentsJson, prettierConfigFile, deployersFileTs);
+  const deployments = parse(DeployersManifest, deploymentsJson);
+  await writeDeployerFactoriesMap(deployments, prettierConfigFile, deployerFactoriesTs);
 }
 
 generate().catch((err) => {
