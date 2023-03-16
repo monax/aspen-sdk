ff5089c8b03494fc624a96afe70ca2974f7b2e6a
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2023-03-16 12:18:43 +0200

    chore: Add chain ids (#68)

--- sdk/src/contracts/network.ts
+++ sdk/src/contracts/network.ts
@@ -7,12 +7,14 @@ export const ChainIdFromChainName = {
   xDai: 100,
   Polygon: 137,
   Moonriver: 1285,
-  Localhost: 1337,
-  Hardhat: 31337,
   Mumbai: 80001,
   Harmony: 1666600000,
   Palm: 11297108109,
   PalmTestnet: 11297108099,
+  Canto: 7700,
+  CantoTestnet: 740,
+  Localhost: 1337,
+  Hardhat: 31337,
 } as const;
 
 export type ChainName = keyof typeof ChainIdFromChainName;
