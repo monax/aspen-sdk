fbb27d3927b3e2cf2f3b43cd17dca37567cd7dc4
Daniel Zahariev <daniel.zahariev@monaxlabs.io>
2024-01-04 10:26:05 +0200

    Add OperatorFilterers

--- sdk/src/contracts/constants.ts
+++ sdk/src/contracts/constants.ts
@@ -1,5 +1,13 @@
+import { ethers } from 'ethers';
+
 export const NATIVE_TOKEN = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
 // FIXME[Silas]: decide what to do with this (config/state/drop)
 export const IPFS_GATEWAY_PREFIX = 'https://ipfs.aspenft.io/ipfs/';
+
+export const OperatorFilterers = {
+  noOperator: ethers.utils.solidityKeccak256(['string'], ['NO_OPERATOR']),
+  openSea: ethers.utils.solidityKeccak256(['string'], ['OPEN_SEA']),
+  blur: ethers.utils.solidityKeccak256(['string'], ['BLUR']),
+};
